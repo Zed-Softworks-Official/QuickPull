3198beaff10e69d8f2327d62fb8f9c973fd3dd26
Kevin Zoltany <kzoltany@gmail.com>
2024-11-09 23:47:24 -0800

    Shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,19 @@ import '~/styles/globals.css'
 
 import { GeistSans } from 'geist/font/sans'
 import { type Metadata } from 'next'
+import Link from 'next/link'
 
 import { TRPCReactProvider } from '~/trpc/react'
+import { ThemeProvider } from '~/components/theme-provider'
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from '~/components/ui/navigation-menu'
+import { ModeToggle } from '~/components/mode-toggle'
 
 export const metadata: Metadata = {
     title: 'QuickPull',
@@ -13,12 +24,37 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en" className={`${GeistSans.variable}`}>
+        <html lang="en" className={`${GeistSans.variable}`} suppressHydrationWarning>
             <body>
-                <TRPCReactProvider>
-                    <main className="container mx-auto">{children}</main>
-                </TRPCReactProvider>
+                <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+                    <TRPCReactProvider>
+                        <main className="container mx-auto w-full">
+                            <Navbar />
+                            {children}
+                        </main>
+                    </TRPCReactProvider>
+                </ThemeProvider>
             </body>
         </html>
+    )
+}
+
+function Navbar() {
+    return (
+        <header className="flex items-center justify-between py-5">
+            <Link href="/">
+                <h1 className="font-bold text-3xl">QuickPull</h1>
+            </Link>
+            <NavigationMenu className="flex items-center gap-2">
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Link href="/upload" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Upload</NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+                <ModeToggle />
+            </NavigationMenu>
+        </header>
     )
 }

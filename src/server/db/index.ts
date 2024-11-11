db2d408ea42b97d1d8d1523a67bb05fae9052488
Kevin Zoltany <kzoltany@gmail.com>
2024-11-10 17:44:06 -0800

    Using Vercel Postgres

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
+import { drizzle } from 'drizzle-orm/vercel-postgres'
 import postgres from 'postgres'
 
 import { env } from '~/env'
@@ -12,7 +12,7 @@ const globalForDb = globalThis as unknown as {
     conn: postgres.Sql | undefined
 }
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL)
+const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL)
 if (env.NODE_ENV !== 'production') globalForDb.conn = conn
 
 export const db = drizzle(conn, { schema })
